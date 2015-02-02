4af7ddcd5b429c5787a067f1b2891da7f71e6bb3
PreZzO88 <daveprezzo@hotmail.com>
2015-02-01 22:00:38 -0500

    changed port

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/node_modules/socket.io'));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.send('Hello World!');
-
+  
 });
 
 app.listen(app.get('port'), function() {
