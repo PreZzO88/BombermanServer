62f9d4f60667cbda605a3152cb3e7396d77b5482
PreZzO88 <daveprezzo@hotmail.com>
2015-02-01 23:51:22 -0500

    sasaas

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var server = http.createServer(function(req, res){
 	res.writeHead(200,{ 'Content-Type': 'text/html' }); 
 	res.end('<h1>Hello Socket Lover!</h1>');
 });
-server.listen(8080);
+server.listen(80);
 
 // Create a Socket.IO instance, passing it our server
 
