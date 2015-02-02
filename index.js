ec914048d5124204bc23de1e295f0693ca630869
PreZzO88 <daveprezzo@hotmail.com>
2015-02-01 21:11:35 -0500

    Test #2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-/*var express = require('express');
-var cool = require('cool-ascii-faces');
+var express = require('express');
+//var cool = require('cool-ascii-faces');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(cool('Hello World!'));
+  response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
