d8bee9f439c7d497a6892e3ccb74c61754c33deb
Dominic Charley-Roy <dominic.charley-roy@trusty>
2015-02-13 14:06:46 -0500

    test123

--- index.js
+++ index.js
@@ -520,7 +520,11 @@ function movePlayer(player, x, y, speed, dir) {
 	updatePlayerCoor(player, x, y, dir, player.altDir, speed);
 	if (!player.broadcastedMoving) {
 		player.broadcastedMoving = true;
-		io.to(player.socketID).broadcast.to(player.gameID).emit('changeDir', { d: { x: player.x, y: player.y, dir: player.dir, altDir: player.altDir }, c: player.color });
+		try {
+			io.to(player.socketID).broadcast.to(player.gameID).emit('changeDir', { d: { x: player.x, y: player.y, dir: player.dir, altDir: player.altDir }, c: player.color });
+		} catch (e) {
+			client.send(e);
+		}
 		//client.broadcast.to(client.gameID).emit('chatmsg', { c: client.gameColor, msg: data.substr(0,300) });
 		//client.to(socketID).emit('layBomb', { r: data.r, c: data.c, o: p.color, s: sync, expStr: p.expStr, i: items });
 	}
